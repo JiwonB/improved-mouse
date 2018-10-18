07d6cbdc85b491062e1ab967022c38ce66e809dd
jiwon <baikjo256@gmail.com>
2018-10-18 11:14:31 +0900

    This is the first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("안녕하세요 \n 반갑습니다");
+                return;
+            }
+            Console.WriteLine("안녕하세요,{0}!", args[0]);
+
+        }
+        
+    }
+}
