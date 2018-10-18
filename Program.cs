5ae0c02aff762e788bf04d93d903f1817c9f872c
jiwon <baikjo256@gmail.com>
2018-10-18 11:18:30 +0900

    This is the second commit

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace greeting
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("안녕하세요 \n 반갑습니다");
+                Console.WriteLine("Hellooo \n 반갑습니다");
                 return;
             }
             Console.WriteLine("안녕하세요,{0}!", args[0]);
